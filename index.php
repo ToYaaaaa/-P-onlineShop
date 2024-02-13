2a2e5d127e1286a4151f7c4866a059d26913bb38
toyaaaaa <farizabdulfatah245@gmail.com>
2024-02-13 11:47:00 +0700

    landing page awal

---
+++ index.php
@@ -0,0 +1,53 @@
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- title -->
+    <title>NexusShop</title>
+    <!-- icon -->
+    <script src="https://unpkg.com/feather-icons"></script>
+    <link rel="shortcut icon" href="picture/iconNoBackground.png" type="image/x-icon">
+    <!-- css -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <!-- navbar-start -->
+    <div class="navbarMain">
+        <a href="index.php" class="navbar-logo">Nexus<span>Shop.</span></a>
+        <div class="navbar-menu">
+            <a href="">Home</a>
+            <a href="">About</a>
+            <a href="">Contact</a>
+            <a href=""><i data-feather="shopping-bag"></i></a>
+        </div>
+        <div class="navbar-extra">
+            <i data-feather="menu"></i>
+        </div>
+
+    </div>
+    <!-- navbar-end -->
+
+    <!-- main page start -->
+    <div class="container">
+    <div class="mainpage"></div>
+    <div class="kata">
+        <p style="font-weight:bold;">#AlwaysBestPrice</p>
+        <h2>"Nexus Shop,Elevate Your Every Day."</h2>
+        <p>Nexus Shop: Style, Quality, Elegance. Explore our exclusive collection, define your unique style.</p>
+        <a>Discover Now</a>
+    </div>
+    </div>
+    <!-- main page end -->
+    <!-- icon -->
+    <script>
+    feather.replace();
+    </script>
+</body>
+</html>
