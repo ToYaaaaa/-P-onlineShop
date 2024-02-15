5ec53245d2961688041a396f561c63cccb3fc63f
toyaaaaa <farizabdulfatah245@gmail.com>
2024-02-15 10:44:15 +0700

    tambah page dan fitur

--- index.php
+++ index.php
@@ -16,6 +16,10 @@
     <link rel="shortcut icon" href="picture/iconNoBackground.png" type="image/x-icon">
     <!-- css -->
     <link rel="stylesheet" href="css/style.css">
+    <!-- font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 </head>
 <body>
     <!-- navbar-start -->
@@ -45,6 +49,97 @@
     </div>
     </div>
     <!-- main page end -->
+
+    <!-- benefit start -->
+    <div class="benefit">
+        <div class="benefit1">
+            <img src="picture/shipping.png" alt="">
+            <h3>Free Shipping</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Nesciunt deleniti voluptatem natus blanditiis dignissimos maiores.</p>
+        </div>
+        <div class="benefit2">
+            <img src="picture/customerService.png" alt="">
+            <h3>Customer Service</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Nesciunt deleniti voluptatem natus blanditiis dignissimos maiores.</p>
+        </div>
+        <div class="benefit3">
+            <img src="picture/securePayment.png" alt="">
+            <h3>Secure Payment</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Nesciunt deleniti voluptatem natus blanditiis dignissimos maiores.</p>
+        </div>
+    </div>
+    <!-- benefit end -->
+
+    <!-- our product start -->
+    <div class="ourproduct">
+            <div class="atas">
+                <h1>Our Product</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, saepe.</p>
+            </div>
+        <div class="bungkusblock">
+            <div class="block1">
+                <div class="bulat1">
+                    <img src="picture/block.png" alt="">
+                </div>
+                <div class="text1">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>RP. 50.000,-</p>
+                    <a href="#"><button>Buy Now</button></a>
+                </div>
+        </div>
+
+        <div class="block2">
+                <div class="bulat2">
+                    <img src="picture/block.png" alt="">
+                </div>
+                <div class="text2">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>RP. 50.000,-</p>
+                    <a href="#"><button>Buy Now</button></a>
+                </div>
+        </div>
+
+        <div class="block3">
+                <div class="bulat3">
+                    <img src="picture/block.png" alt="">
+                </div>
+                <div class="text3">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>RP. 50.000,-</p>
+                    <a href="#"><button>Buy Now</button></a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- our product end -->
+
+    <!-- deal of the month start -->
+    <div class="deal">
+        <img src="picture/block.png" alt="">
+        <div class="textatas">
+            <p>Deal Of The Month</p>
+            <h1>Deal Of The Month</h1>
+        </div>
+        <div class="times">
+            <div id="day"></div>
+            <div id="hour"></div>
+            <div id="minute"></div>
+            <div id="second"></div>
+        </div>
+        <div class="price">
+            <h1>Lorem ipsum dolor sit amet.</h1>
+            <h2>RP. 50.000,-</h2>
+            <img src="picture/block.png" alt="">
+            <img src="picture/block.png" alt="">
+            <img src="picture/block.png" alt="">
+        </div>
+
+    </div>
+    <!-- deal of the month end -->
+
     <!-- icon -->
     <script>
     feather.replace();
